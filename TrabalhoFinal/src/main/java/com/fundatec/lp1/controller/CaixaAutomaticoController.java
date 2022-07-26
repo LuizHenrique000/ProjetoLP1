fc7d6033b6f6a41f5c5b41d594bfe1ebb41441c2
LuizHenrique000 <luizlhra24@gmail.com>
2022-07-26 20:25:01 -0300

    Implementa service e controller do caixaAutomatico

--- TrabalhoFinal/src/main/java/com/fundatec/lp1/controller/CaixaAutomaticoController.java
+++ TrabalhoFinal/src/main/java/com/fundatec/lp1/controller/CaixaAutomaticoController.java
@@ -3,8 +3,10 @@ package com.fundatec.lp1.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.fundatec.lp1.models.Conta;
 import com.fundatec.lp1.service.CaixaService;
 
 @RestController
@@ -13,10 +15,16 @@ public class CaixaAutomaticoController {
 
 	@Autowired
 	private CaixaService service;
-	
+
 	@GetMapping(value = "/saldo/{senha}/{id}")
 	public Double consultarSaldo(@PathVariable Integer senha, @PathVariable Integer id) {
-		return service.consultarSaldo(senha,id);
+		return service.consultarSaldo(senha, id);
 	}
-	
+
+	@PutMapping(value = "deposito/{senha}/{id}/{deposito}")
+	public Conta depositar(@PathVariable Integer senha, @PathVariable Integer id, @PathVariable Double deposito) {
+		return service.depositar(senha, id, deposito);
+
+	}
+
 }
--- TrabalhoFinal/src/main/java/com/fundatec/lp1/service/CaixaService.java
+++ TrabalhoFinal/src/main/java/com/fundatec/lp1/service/CaixaService.java
@@ -2,6 +2,7 @@ package com.fundatec.lp1.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import com.fundatec.lp1.models.Conta;
 import com.fundatec.lp1.repository.ContaRepository;
 import com.fundatec.lp1.service.exceptions.EntityNotFoundException;
@@ -9,20 +10,40 @@ import com.fundatec.lp1.service.exceptions.SenhaInvalidaException;
 
 @Service
 public class CaixaService {
-	
+
 	@Autowired
 	private ContaRepository repository;
-	
-	public Double consultarSaldo(Integer senha,Integer id) {
+
+	public Double consultarSaldo(Integer senha, Integer id) {
 		Conta conta = repository.findById(id).orElseThrow(() -> new EntityNotFoundException("Conta inexistente"));
-		Integer senhaConta = conta.getSenha();
+		validar(senha, conta);
 		Double saldo = conta.getSaldo();
-		if (senhaConta == senha) {
-			return saldo;
-		} else {
-			throw new SenhaInvalidaException("Senha inválida");
-		}
-		
+		return saldo;
+
+	}
+
+	public Conta depositar(Integer senha, Integer id, Double deposito) {
+		Conta conta = repository.findById(id).orElseThrow(() -> new EntityNotFoundException("Conta inexistente"));
+		validar(senha, conta);
+		Double saldo = conta.getSaldo() + deposito;
+		conta.setSaldo(saldo);
+		return repository.save(conta);
 	}
 	
+	public Double sacar(Integer senha, Integer id, Double saque) {
+		Conta conta = repository.findById(id).orElseThrow(() -> new EntityNotFoundException("Conta inexistente"));
+		validar(senha, conta);
+		Double saldo = conta.getSaldo() - saque;
+		conta.setSaldo(saldo);
+		repository.save(conta);
+		return saque;
+	}
+
+	public void validar(Integer senha, Conta conta) {
+		Integer senhaConta = conta.getSenha();
+		if (senhaConta.intValue() != senha.intValue()) {
+			throw new SenhaInvalidaException("Senha invalida");
+		}
+	}
+
 }
