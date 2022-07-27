ee8d4e1443fb30f9b1c9ed3aeb77b0d12546e33b
LuizHenrique000 <luizlhra24@gmail.com>
2022-07-26 22:06:04 -0300

    Deleta caixaAutomatico

--- CaixaAutomatico/src/main/java/com/fundatec/lp1/enums/StatusConta.java
+++
@@ -1,6 +0,0 @@
-package com.fundatec.lp1.enums;
-
-public enum StatusConta {
-
-	ATIVA, INATIVA, INDEFINIDA;
-}
--- CaixaAutomatico/src/main/java/com/fundatec/lp1/enums/TipoCliente.java
+++
@@ -1,7 +0,0 @@
-package com.fundatec.lp1.enums;
-
-public enum TipoCliente {
-
-	TITULAR, NAO_TITULAR;
-
-}
